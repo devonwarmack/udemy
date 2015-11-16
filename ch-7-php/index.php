c3d21d3d2431c8725066d416eaf447b1a7334c74
Devon Warmack <devonwarmack@karmaconsulting.com>
2015-11-16 00:33:19 -0600

    fix form project

--- ch-7-php/index.php
+++ ch-7-php/index.php
@@ -1,43 +1,53 @@
 <?php
-    if ($_POST["submit"]) {
-        if (!$_POST['name']) {
-            $error.="<br />Enter Your Name";
-        }
-
-        if (!$_POST['email']) {
-            $error.="<br />Enter Your Email Address";
-        }
-
-        if (!$_POST['comment']_ {
-            $error.="<br />Comment is manditory";
-        }
-
-        if ($_POST['email']!="" AND !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-            $errror.="<br />The Email Address You Entered Was Not Valid";
-        }
-
-        if (mail("test@gmail.com", "Comments", "Name: ".$_POST['name']."
-
-            Email: ".$_POST['email']."
-
-            Comment: ".$_POST['comment'])) {
-                $result='<div class="alert alert-success">
-                    <strong>Thanks.</strong>I'll get back with you.
-                </div>';
+        if ($_POST["submit"]) {
+            if (!$_POST['name']) {
+                $error.="<br />Enter Your Name";
             }
-        else {
-            $result='<div class="alert alert-danger">
-                Message could not be sent. Try again later.
-            </div>';
+
+            if (!$_POST['email']) {
+                $error.="<br />Enter Your Email Address";
+            }
+
+            if (!$_POST['comment']) {
+                $error.="<br />Comment is manditory";
+            }
+
+            if ($_POST['email']!="" AND !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $error.="<br />The Email Address You Entered Was Not Valid";
+            }
+
+            if ($error) {
+                $result='<div class="alert alert-danger"><strong>'.
+                    'Fill the form out correctly</strong>'.$error.
+                '</div>';
+
+             }
+
+             else {
+
+                if (mail("test@gmail.com", "Comments", "Name: ".$_POST['name']."
+
+                    Email: ".$_POST['email']."
+
+                    Comment: ".$_POST['comment'])) {
+                        $result='<div class="alert alert-success">
+                             <strong>Thanks.</strong>I\'ll get back with you.
+                        </div>';
+                    }
+                else {
+                    $result='<div class="alert alert-danger">
+                        Message could not be sent. Try again later.
+                    </div>';
+                }
+             }
         }
-    }
-}
 
 
-?>
-
+    ?>
 
 <!doctype html>
+
+
 <html>
 <head>
     <title>Learning jquery</title>
@@ -46,7 +56,7 @@
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha51
     2-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
@@ -54,6 +64,7 @@
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity=
     "sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+
 
 
 </head>
@@ -72,13 +83,11 @@
                         <div class="form-group">
                             <label for="email">Your Email Address:</label>
                             <input type="email" name="email" class="form-control" placeholder="Your Email Address"
-                                   value="<php echo $_POST['email']; ?>" />
+                            value="<?php echo $_POST['email']; ?>" />
                         </div>
                     <div class="form-group">
                         <label for="comment">Any Comments?</label>
-                        <textarea class="form-control" name="comment">
-                            <?php echo $_POST['comment']; ?>
-                        </textarea>
+                        <textarea class="form-control" name="comment"><?php echo $_POST['comment']; ?></textarea>
                     </div>
                 </form>
             </div>
